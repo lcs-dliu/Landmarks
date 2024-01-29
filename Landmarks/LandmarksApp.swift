1e2cd588fd0b45598498060d5afd13936e5c1916
Dawn Liu <dliu@lcs.on.ca>
2024-01-29 11:15:05 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Chenxi Liu on 2024/1/29.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
